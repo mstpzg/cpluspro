db8ea3e08311844a95c4af51e13d151331b3fc00
mstpzg <mstpzg@gmail.com>
2018-09-16 21:40:21 +0200

    pseudo_shell commit

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<string.h>
+#define MAXLINE 4096
+int main()
+{
+    char    buf[MAXLINE];
+    pid_t   pid;
+    int status;
+
+    printf("%% "); //znak zachęty
+    while(fgets(buf,MAXLINE,stdin)!=NULL){
+        buf[strlen(buf) - 1] = 0; // zastępujemy znak nowego wiersza pustym znakiem
+    if((pid=fork()) < 0)
+            printf("fork error");
+    else if(pid == 0)
+        {//proces potomnY
+            execlp(buf,buf,(char*)0);
+            printf("Nie mozna uruchomic: %s",buf);
+            printf("\n");
+            exit(127);
+        }
+    //proces macierzysty
+    if( (pid=waitpid(pid, &status, 0) ) < 0)
+        printf("waitpid error");
+        printf("%% ");
+    }
+
+    return 0;
+}
